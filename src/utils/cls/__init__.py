4102b2490c0b65cda51fb63cfb970d0f7b83d775
ustceewy <118595575+ustceewy@users.noreply.github.com>
2025-03-24 20:10:52 +0800

    Update __init__.py

--- src/utils/cls/__init__.py
+++ src/utils/cls/__init__.py
@@ -3,6 +3,7 @@ from .metrics import calculate_accuracy
 from .misc import SmoothedValue, _get_cache_path, collate_fn
 from .presets import PresetTrain, PresetEval
 
+
 __all__ = [
     # logger.py
     'MetricLogger',
